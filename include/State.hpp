d2e89b87f30548348daf8048cecf6b056bde2c46
Im-Abhi <abhishekgupta942001@gmail.com>
2021-12-04 10:51:22 +0530

    Create game state class

---
+++ include/State.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML/System/Time.hpp>
+
+using namespace sf;
+
+namespace Engine
+{
+    class State
+    {
+    public:
+        State();
+        virtual ~State();
+        
+        virtual void Init() = 0;    //for setting up the sprites text and thier positions
+        virtual void ProcessInput() = 0; // handle keyPress and mouse clicks
+        virtual void Update(Time deltaTime) = 0; 
+        virtual void Draw() = 0; // For drawing sprites on the game window
+
+        virtual void Pause(){};
+        virtual void Start(){};
+    };
+} // namespace Engine
