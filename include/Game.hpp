1406ed55a668584535cbdf79d10cfba0ed74b0ba
Faiz Alaam <faizalaam@Faizs-MacBook-Pro.local>
2021-12-12 13:01:08 +0530

    menu files

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-#include "AssetMan.hpp"
-#include "StateMan.hpp"
-
 #include <memory>
 
 #include <SFML/Graphics/RenderWindow.hpp>
+
+#include "AssetMan.hpp"
+#include "StateMan.hpp"
+
+enum AssetID
+{
+    MAIN_FONT = 0,
+    GRASS,
+    FOOD,
+    WALL,
+    SNAKE
+};
 
 struct Context
 {
@@ -21,14 +30,11 @@ struct Context
     }
 };
 
-//  Game Class
 class Game
 {
 private:
-
     std::shared_ptr<Context> m_context;
-
-    const sf::Time TIME_PER_FRAME = sf::seconds(1.f / 60.f);
+    const sf::Time TIME_PER_FRAME = sf::seconds(1.f/60.f);
 
 public:
     Game();
