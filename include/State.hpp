9b4870e45e2a556c7b5e8eb5e45bc9f6718542b6
Im-Abhi <abhishekgupta942001@gmail.com>
2021-12-29 20:36:00 +0530

    Add comments
    Comments added to state and stateman class

--- include/State.hpp
+++ include/State.hpp
@@ -10,12 +10,24 @@ namespace Engine
         State(){};
         virtual ~State(){};
 
+        // Initial setup of state like setting up the sprites textures and text
         virtual void Init() = 0;
+
+        // Process any keyboard or mouse input provided by the user
         virtual void ProcessInput() = 0;
+
+        /*         
+            This will react to the inputs handled in the processInput method and will make changes in the state.
+        */        
         virtual void Update(sf::Time deltaTime) = 0;
+        
+        // This will draw all the sprites on the render Window
         virtual void Draw() = 0;
 
+        // This method will help us to pause the game
         virtual void Pause(){};
+
+        // This method will help us to resume and play the game
         virtual void Start(){};
     };
     
--- include/StateMan.hpp
+++ include/StateMan.hpp
@@ -10,7 +10,10 @@ namespace Engine
 class StateMan
 {
 private:
+    // This creates a state stack which will hold all the states while game is running
     std::stack<std::unique_ptr<State>> m_stateStack;
+
+    // This will be used to add new state to the stack only when the previous state is completed
     std::unique_ptr<State> m_newState;
 
     bool m_add;
@@ -21,9 +24,16 @@ public:
     StateMan();
     ~StateMan();
 
+    // This method is used to push the new state to the state stack
     void Add(std::unique_ptr<State> toAdd, bool replace = false);
+
+    // This method will pop or remove the top state from the state stack
     void PopCurrent();
+
+    // In this mehtod actual process will take place like push and pop
     void ProcessStateChange();
+
+    // This will be used to get the current game state from the state stack
     std::unique_ptr<State>& GetCurrent();
 };
 
